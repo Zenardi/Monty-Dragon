1217a602ee3521eae2bfe7170be8c81d3ba20fdb
Eduardo Doval Zenardi Filho <du.zenardi@gmail.com>
2017-09-08 23:24:14 -0300

    Fix Player Movement

--- Assets/_Characters/CharacterMovement.cs
+++ Assets/_Characters/CharacterMovement.cs
@@ -8,23 +8,29 @@ namespace RPG.Characters
     public class CharacterMovement : MonoBehaviour
     {
         [SerializeField] float stopDistance = 1.0f;
+        [SerializeField] float moveSpeedMultiplier = 0.7f;
 
         ThirdPersonCharacter character;   // A reference to the ThirdPersonCharacter on the object
         
         Vector3 clickPoint;
         GameObject walkTarget;
         NavMeshAgent agent;
+        Animator animator;
+        Rigidbody myRigidbody;
+
         void Start()
         {
             CameraUI.CameraRaycaster cameraRaycaster = Camera.main.GetComponent<CameraUI.CameraRaycaster>();
             character = GetComponent<ThirdPersonCharacter>();
+            myRigidbody = GetComponent<Rigidbody>();
+            animator = GetComponent<Animator>();
+
             walkTarget = new GameObject("walkTarget");
 
             agent = GetComponent<NavMeshAgent>();
             agent.updatePosition = true ;
             agent.updateRotation = false;
             agent.stoppingDistance = stopDistance;
-
 
             cameraRaycaster.onMouseOverPotentiallyWalkable += OnMouseOverPotentiallyWalkable;
             cameraRaycaster.onMouseOverEnemy += OnMouseOverEnemy;
@@ -55,6 +61,20 @@ namespace RPG.Characters
             if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(1))
             {
                 agent.SetDestination(enemy.transform.position);
+            }
+        }
+
+        public void OnAnimatorMove()
+        {
+            // we implement this function to override the default root motion.
+            // this allows us to modify the positional speed before it's applied.
+            if (Time.deltaTime > 0)
+            {
+                Vector3 velocity = (animator.deltaPosition * moveSpeedMultiplier) / Time.deltaTime;
+
+                // we preserve the existing y part of the current velocity.
+                velocity.y = myRigidbody.velocity.y;
+                myRigidbody.velocity = velocity;
             }
         }
     }
--- Assets/_Characters/ThirdPersonCharacter.cs
+++ Assets/_Characters/ThirdPersonCharacter.cs
@@ -9,7 +9,6 @@ namespace RPG.Characters
 	{
 		[SerializeField] float movingTurnSpeed = 360;
 		[SerializeField] float stationaryTurnSpeed = 180;
-		[SerializeField] float runCycleLegOffset = 0.2f; //specific to the character in sample assets, will need to be modified to work with others
 		
 		Rigidbody myRigidbody;
 		Animator animator;
@@ -53,16 +52,6 @@ namespace RPG.Characters
 			// update the animator parameters
 			animator.SetFloat("Forward", m_ForwardAmount, 0.1f, Time.deltaTime);
 			animator.SetFloat("Turn", m_TurnAmount, 0.1f, Time.deltaTime);
-			//m_Animator.SetBool("Crouch", m_Crouching);
-			//m_Animator.SetBool("OnGround", m_IsGrounded);
-
-
-			// calculate which leg is behind, so as to leave that leg trailing in the jump animation
-			// (This code is reliant on the specific run cycle offset in our animations,
-			// and assumes one leg passes the other at the normalized clip times of 0.0 and 0.5)
-			float runCycle =
-				Mathf.Repeat(
-					animator.GetCurrentAnimatorStateInfo(0).normalizedTime + runCycleLegOffset, 1);		
 		}
 
 		void ApplyExtraTurnRotation()
@@ -71,6 +60,8 @@ namespace RPG.Characters
 			float turnSpeed = Mathf.Lerp(stationaryTurnSpeed, movingTurnSpeed, m_ForwardAmount);
 			transform.Rotate(0, m_TurnAmount * turnSpeed * Time.deltaTime, 0);
 		}
-        
-	}
+
+
+
+    }
 }
